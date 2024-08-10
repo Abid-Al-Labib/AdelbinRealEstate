a6132ca1005dd5f7717ca0fcea6e1207f05a7878
Abid-Al-Labib <getabidallabib@gmail.com>
2024-08-09 23:44:01 -0400

    vercel deployment error fixes

--- src/components/MenuCard.tsx
+++ src/components/MenuCard.tsx
@@ -6,7 +6,7 @@ interface MenuCardProps {
     subtext: string;
   }
   
-  const MenuCard: FC<MenuCardProps> = ({ iconURL, label, subtext }) => {
+  const MenuCard: FC<MenuCardProps> = ({ iconURL, label }) => {
     return (
       <div className="flex flex-col flex-1 border-2 rounded-[20px] shadow-3xl px-10 py-10 hover:border-crimson hover:shadow-gray-600">
         <div className="w-10 h-10 flex justify-center items-center bg-crimson rounded-full">
