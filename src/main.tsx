2ee31ddb15e92957c2a23d94790342928554a4e5
Abid-Al-Labib <getabidallabib@gmail.com>
2024-08-09 23:37:54 -0400

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,3 @@
-// import * as React from "react";
-// import * as ReactDOM from "react-dom/client";
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-// import "./index.css";
-// import HomePage from "./pages/Homepage";
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage/>,
-//   },
-// ]);
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
