d30586f8f9be367c1bb2bf320c7e0601ab5ad754
Abid-Al-Labib <getabidallabib@gmail.com>
2024-08-09 23:58:22 -0400

    hero section padding adjustments

--- src/sections/ConstructionHero.tsx
+++ src/sections/ConstructionHero.tsx
@@ -8,15 +8,15 @@ const ConstructionHero = () => {
         max-container w-full bg-blue-950 bg-cover bg-center"
     >
         <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-15">
-            <h1 className="text-orange-300 pl-4 mt-10 font-palanquin text-4xl sm:text-8xl  font-bold ">
+            <h1 className="text-orange-300 pl-0 md:pl-3 mt-10 font-palanquin text-4xl sm:text-8xl  font-bold ">
                 <span>Construction Services</span>
                 <br />          
             </h1>
-                <p className="font-montserrat pl-6 text-white text-base sm:text-lg leading-8 mt-12 sm:min-w-min">
+                <p className="font-montserrat pl-0 md:pl-5 text-white text-base sm:text-lg leading-8 mt-12 sm:min-w-min">
                     Licensed General Contractor Serving New York City & Nassau County.
                 </p>
 
-                <p className="font-montserrat pl-6 text-white text-base sm:text-lg leading-8 sm:min-w-min">
+                <p className="font-montserrat pl-0 md:pl-5 text-white text-base sm:text-lg leading-8 sm:min-w-min">
                     Delivering exceptional quality and innovative practices to ensure your project stands the test of time, all at the best price. 
                 </p>     
         </div>
--- src/sections/RealEstateHero.tsx
+++ src/sections/RealEstateHero.tsx
@@ -8,17 +8,17 @@ const RealEstateHero = () => {
         max-container w-full bg-blue-950 bg-cover bg-center"
     >
         <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-15">
-            <h1 className="text-orange-300 pl-10 mt-10 font-palanquin text-[95px] max-sm:text-[70px] max-sm:leading-[78px] font-bold">
+            <h1 className="text-orange-300 pl-0 md:pl-10 mt-10 font-palanquin text-[95px] max-sm:text-[70px] max-sm:leading-[78px] font-bold">
                 <span>Real Estate</span>        
             </h1>
-            <p className="whitespace-nowrap font-montserrat pl-10 text-white text-xl sm:text-2xl font-bold leading-8 mt-6 sm:max-w-sm">
+            <p className="whitespace-nowrap font-montserrat pl-0 md:pl-10 text-white text-xl sm:text-2xl font-bold leading-8 mt-6 sm:max-w-sm">
                     Residential And Commercial Sales
             </p>
-            <p className="font-montserrat pl-10 text-white text-base sm:text-lg leading-8 mt-6 sm:min-w-min">
+            <p className="font-montserrat pl-0 md:pl-10 text-white text-base sm:text-lg leading-8 mt-6 sm:min-w-min">
                 Whether you’re buying or selling real estate, with Adelbin, you can be confident you’ll secure the best deals always.
             </p>
 
-            <p className="font-montserrat pl-10 mt-1 text-white text-base sm:text-lg leading-8 sm:min-w-min">
+            <p className="font-montserrat pl-0 md:pl-10 mt-1 text-white text-base sm:text-lg leading-8 sm:min-w-min">
                 Your confidence is guaranteed through actions, find a partner in Adelbin and help your bottom line. 
             </p>     
         </div>
